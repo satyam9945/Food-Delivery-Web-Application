2c4c4c33d307e02f57a605d9c10f0a69f8fae072
Satyam <satyamgupta8824@gamil.com>
2026-05-11 17:38:01 +0530

    add cloudinary

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,7 +1,12 @@
 import foodModel from "../models/foodModel.js";
-import fs from 'fs'
+import {v2 as cloudinary} from 'cloudinary';
 
-// all food list
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const listFood = async (req, res) => {
     try {
         const foods = await foodModel.find({})
@@ -10,21 +15,18 @@ const listFood = async (req, res) => {
         console.log(error);
         res.json({ success: false, message: "Error" })
     }
-
 }
 
-// add food
 const addFood = async (req, res) => {
-
     try {
-        let image_filename = `${req.file.filename}`
-
+        const result = await cloudinary.uploader.upload(req.file.path);
+        
         const food = new foodModel({
             name: req.body.name,
             description: req.body.description,
             price: req.body.price,
-            category:req.body.category,
-            image: image_filename,
+            category: req.body.category,
+            image: result.secure_url,
         })
 
         await food.save();
@@ -35,21 +37,14 @@ const addFood = async (req, res) => {
     }
 }
 
-// delete food
 const removeFood = async (req, res) => {
     try {
-
-        const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`, () => { })
-
         await foodModel.findByIdAndDelete(req.body.id)
         res.json({ success: true, message: "Food Removed" })
-
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Error" })
     }
-
 }
 
 export { listFood, addFood, removeFood }
